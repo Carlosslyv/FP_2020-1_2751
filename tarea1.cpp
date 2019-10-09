4f3d59c1e40a547d3b8524258d790ac816bd6e67
Carlosslyv <54519877+Carlosslyv@users.noreply.github.com>
2019-10-08 23:17:14 -0500

    Add files via upload

---
+++ tarea1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	char caracter [1];
+	scanf ("%s", &caracter);
+	printf("%s\n", caracter);
+	
+	return 0;
+}
+
---
+++ tarea2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main (int argc, char const *argv[])
+
+{
+	int x;
+	scanf("%d", &x);
+	if (x%2==0);
+	    
+	        printf("es par\n");
+
+	
+	    printf ("es impar\n");
+	
+	return 0;
+}
+
