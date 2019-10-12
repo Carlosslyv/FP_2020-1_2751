e58537de68fc931a237e5dc2c4b5921f1fa8f8aa
Carlosslyv <Carlos.sotley2708@gmail.com>
2019-10-11 19:19:59 -0500

    hola

---
+++ TAREAS/2/main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main (int argc, char*argv[]){
+	int x;
+	
+	x=atoi(argv[1]);
+	
+	if(x%2==0){
+		printf("1\n");
+	}
+	else{
+		printf("0\n");
+    }
+	return 0;   
+}
+
